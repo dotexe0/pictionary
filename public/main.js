5308a511f9ca354f42adbeee33f5b07596f01ee8
dotexe0 <dv@Daniels-MBP.att.net>
2016-12-28 16:07:23 -0800

    guess option working on emit

--- public/main.js
+++ public/main.js
@@ -1,24 +1,66 @@
+var socket = io();
+
 $(document).ready(function() {
-  pictionary();
-});
+  var pictionary = function() {
+    var canvas = $('canvas');
+    var context;
+    var drawing = false;
+    var guessBox;
 
-var pictionary = function() {
-  var canvas = $('#canvas');
-  var context;
+    var previousGuesses = function(guess) {
+      $('#previous-guesses').text("Previous guess: " + guess);
+    }
 
-  var draw = function(position) {
-    context.beginPath();
-    context.arc(position.x, position.y, 6, 0, 2 * Math.PI);
-    context.fill();
+    var onKeyDown = function(event) {
+        if (event.keyCode != 13) { // Enter
+            return;
+        }
+        var guess = guessBox.val();
+        console.log(guess);
+        previousGuesses(guess);
+        socket.emit('guess', guess);
+        guessBox.val('');
+    };
+
+    guessBox = $('#guess input');
+    guessBox.on('keydown', onKeyDown);
+
+    var draw = function(position) {
+        context.beginPath();
+        context.arc(position.x, position.y,
+                         6, 0, 2 * Math.PI);
+        context.fill();
+    };
+
+    context = canvas[0].getContext('2d');
+    canvas[0].width = canvas[0].offsetWidth;
+    canvas[0].height = canvas[0].offsetHeight;
+
+    canvas.on('mousedown', function(e) {
+      drawing = true;
+      console.log("mouse is down");
+    });
+
+    canvas.on('mouseup', function(e) {
+      drawing = false;
+      console.log("mouse is UP!");
+    });
+
+     canvas.on('mouseover', function(e) {
+       if (drawing == true) {
+        var offset = canvas.offset();
+        var position = {x: e.pageX - offset.left,
+                        y: e.pageY - offset.top };
+        console.log(position);
+        draw(position);
+        socket.emit('draw', position);
+        }
+    });
+
+
+    socket.on('draw', draw);
+    socket.on('guess', previousGuesses);
   };
 
-  context = canvas[0].getContext('2d');
-  canvas[0].width = canvas[0].offsetWidth;
-  canvas[0].height = canvas[0].offsetHeight;
-  canvas.on('mouseover', function(e) {
-    var offset = canvas.offset();
-    var position = {x: e.pageX - offset.length,
-                    y: e.pageY - offset.top };
-    draw(position);
-  });
-};
+  pictionary();
+});
--- server.js
+++ server.js
@@ -5,8 +5,20 @@ var socket_io = require('socket.io');
 var app = express();
 app.use(express.static('public'));
 
-var server = http.Server('public');
+var server = http.Server(app);
 var io = socket_io(server);
+
+io.on('connection', function(socket) {
+  socket.on('draw', function(position) {
+    socket.broadcast.emit('draw', position);
+    console.log('server drawing working');
+  });
+
+  socket.on('guess', function(guess) {
+    io.emit('guess', guess);
+    console.log("The guess was :", guess);
+  })
+});
 
 server.listen(process.env.PORT || 8080);
 console.log('Running on localhost:8080...');
